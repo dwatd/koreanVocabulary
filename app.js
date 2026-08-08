bc11492d4bf4421382235f4a78efae39deed0cfb
Kateryna Semeniuk <semenyukkate4@gmail.com>
2026-08-08 20:15:31 +0300

    add style.css

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { engine } = require('express-handlebars');
 const { spawn } = require('child_process');
 
 const app = express()
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 const path = require('path')
 
